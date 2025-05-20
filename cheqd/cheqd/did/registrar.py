893ea6782c9e57be25cdd34b8a88d1411175691a
ff137 <ff137@proton.me>
2025-05-20 13:02:02 +0300

    :bug: Force 1 second sleep between sequential requests, and retry if account sequence mismatch occurs

--- cheqd/cheqd/did/registrar.py
+++ cheqd/cheqd/did/registrar.py
@@ -1,7 +1,9 @@
 """DID Registrar for Cheqd."""
 
 import logging
+import asyncio
 from asyncio import Lock
+from time import time
 
 from aiohttp import ClientSession
 from pydantic import BaseModel, ValidationError
@@ -34,33 +36,71 @@ class DIDRegistrar(BaseDIDRegistrar):
             self.DID_REGISTRAR_BASE_URL = registrar_url
         self.method = method
         self._lock = Lock()
+        self._last_request_time = 0
 
     async def _execute_request(self, endpoint: str, options: BaseModel) -> dict:
         """Execute a request to the DID Registrar."""
         async with self._lock:  # Ensure that only one request is processed at a time
+            current_time = time()
+            time_since_last_request = current_time - self._last_request_time
+            if time_since_last_request < 1:
+                LOGGER.debug(
+                    "Sleeping for %s seconds before next Cheqd DID Registrar request",
+                    1 - time_since_last_request,
+                )
+                await asyncio.sleep(1 - time_since_last_request)
+            self._last_request_time = time()
+
             async with ClientSession() as session:
-                try:
-                    async with session.post(
-                        f"{self.DID_REGISTRAR_BASE_URL}{endpoint}?method={self.method}",
-                        json=options.model_dump(exclude_none=True),
-                    ) as response:
-                        try:
-                            res = await response.json()
-                        except Exception:
+                return await self._retry_request(session, endpoint, options)
+
+    async def _retry_request(
+        self, session: ClientSession, endpoint: str, options: BaseModel, retries: int = 2
+    ) -> dict:
+        """Retry logic for executing a request."""
+        while retries >= 0:
+            try:
+                async with session.post(
+                    f"{self.DID_REGISTRAR_BASE_URL}{endpoint}?method={self.method}",
+                    json=options.model_dump(exclude_none=True),
+                ) as response:
+                    res = await self._parse_response(response, endpoint)
+                    if self._should_retry(res):
+                        if retries > 0:
+                            LOGGER.info("Account sequence mismatch, retrying...")
+                            await asyncio.sleep(1)
+                            retries -= 1
+                            continue
+                        else:
                             raise DIDRegistrarError(
-                                f"cheqd: did-registrar: {endpoint}: Unable to parse JSON"
+                                f"cheqd: did-registrar: {endpoint}: "
+                                "Account sequence mismatch after retries."
                             )
-                        if not res:
-                            raise DIDRegistrarError(
-                                f"cheqd: did-registrar: {endpoint}: Response is None."
-                            )
-                        return res
-                except (ValidationError, AttributeError):
-                    raise DIDRegistrarError(
-                        f"cheqd: did-registrar: {endpoint}: Response Format is invalid"
-                    )
-                except Exception:
-                    raise
+                    return res
+            except (ValidationError, AttributeError):
+                raise DIDRegistrarError(
+                    f"cheqd: did-registrar: {endpoint}: Response Format is invalid"
+                )
+            except Exception:
+                raise
+
+    async def _parse_response(self, response, endpoint: str) -> dict:
+        """Parse the response from the DID Registrar."""
+        try:
+            res = await response.json()
+        except Exception:
+            raise DIDRegistrarError(
+                f"cheqd: did-registrar: {endpoint}: Unable to parse JSON"
+            )
+        if not res:
+            raise DIDRegistrarError(
+                f"cheqd: did-registrar: {endpoint}: Response is None."
+            )
+        return res
+
+    def _should_retry(self, res: dict) -> bool:
+        """Determine if the request should be retried based on the response."""
+        return "description" in res and "account sequence mismatch" in res["description"]
 
     async def create(
         self, options: DidCreateRequestOptions | SubmitSignatureOptions
