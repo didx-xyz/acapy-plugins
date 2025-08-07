2effc1bd1445a2c22d33723ca9b67890679af9bf
ff137 <ff137@proton.me>
2025-08-07 18:59:03 +0200

    :art: Move resource recovery logs to info level

--- cheqd/cheqd/anoncreds/registry.py
+++ cheqd/cheqd/anoncreds/registry.py
@@ -432,7 +432,7 @@ class DIDCheqdRegistry(BaseAnonCredsResolver, BaseAnonCredsRegistrar):
             (_, resource_id) = self.split_did_url(revocation_registry_definition_id)
         except AnonCredsRegistrationError as e:
             if "Resource already exists" in str(e):
-                LOGGER.debug(
+                LOGGER.info(
                     "Resource already exists, fetching existing revocation registry definition"
                 )
                 # Fetch the existing resource using resourceType and resourceName
@@ -452,7 +452,7 @@ class DIDCheqdRegistry(BaseAnonCredsResolver, BaseAnonCredsRegistrar):
                     )
                 revocation_registry_definition_id = f"{did}/resources/{resource_id}"
 
-                LOGGER.debug(
+                LOGGER.info(
                     "Found existing revocation registry definition ID, try to fetch: %s.",
                     revocation_registry_definition_id,
                 )
@@ -594,7 +594,7 @@ class DIDCheqdRegistry(BaseAnonCredsResolver, BaseAnonCredsRegistrar):
             (_, resource_id) = self.split_did_url(did_url)
         except AnonCredsRegistrationError as e:
             if "Resource already exists" in str(e):
-                LOGGER.debug(
+                LOGGER.info(
                     "Resource already exists, fetching existing revocation list"
                 )
                 # Fetch the existing resource using resourceType and resourceName
@@ -614,7 +614,7 @@ class DIDCheqdRegistry(BaseAnonCredsResolver, BaseAnonCredsRegistrar):
                     )
                 did_url = f"{did}/resources/{resource_id}"
 
-                LOGGER.debug(
+                LOGGER.info(
                     "Found existing revocation list ID: %s.",
                     did_url,
                 )
